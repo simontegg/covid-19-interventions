704f900e24ea0a3896b8e38c6e0e401e0279e23f
Simon Tegg <simontegg@gmail.com>
2020-05-31 16:52:10 +1200

    concat jurisdiction name

--- index.R
+++ index.R
@@ -1,8 +1,19 @@
-interventions <- read.csv(file = "./interventions.csv")
-tpmControlEnd <- as.Date("2020-04-17")
-tpm <- subset(interventions, (!is.na(interventions$TPM_early_phase_r_eff)))
+# library(tidyverse)
 
-print(int$country_code)
+# data
+interventions <- read.csv(file = "./interventions.csv", na.strings=c("","NA"))
+
+# constants
+tpmControlEnd <- as.Date("2020-04-17")
+
+
+# Te Pūnaha Matatini model
+tpm <- subset(interventions, (!is.na(interventions$TPM_early_phase_r_eff)))
+tpm$region[tpm$region == ""] <- NA
+tpm$jurisdiction <-  paste(tpm$country_code, tpm$region, sep="_")
+tpm$jurisdiction <- gsub("_NA", "", tpm$jurisdiction)
+
+print(tpm$jurisdiction)
 
 
 
