d6f0690f9845b60fc7b13cc2af567c64bd7deab5
Simon Tegg <simontegg@gmail.com>
2020-05-31 16:08:56 +1200

    init

---
+++ index.R
@@ -0,0 +1,12 @@
+interventions <- read.csv(file = "./interventions.csv")
+tpmControlEnd <- as.Date("2020-04-17")
+tpm <- subset(interventions, (!is.na(interventions$TPM_early_phase_r_eff)))
+
+print(int$country_code)
+
+
+
+
+
+
+
